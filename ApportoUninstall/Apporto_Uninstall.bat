9737d14d9815adbfddf011cb01f130fa378d0c97
dcfisher <dcfisher@oakland.edu>
2016-09-28 12:10:13 -0400

    Supressed the errors to make it run cleaner

--- ApportoUninstall/Apporto_Uninstall.bat
+++ ApportoUninstall/Apporto_Uninstall.bat
@@ -2,21 +2,21 @@
 
 echo "Uninstalling Apporto"
 echo "Killing Apporto processes"
-taskkill /f /im BizAppCenterService.exe
-taskkill /f /im ApportoSystray.exe
-sc stop BizAppCenterService
-sc delete BizAppCenterService
+taskkill /f /im BizAppCenterService.exe 
+taskkill /f /im ApportoSystray.exe 
+sc stop BizAppCenterService >nul
+sc delete BizAppCenterService >nul
 
-NET USE Z: /delete
-NET USE /persistent:yes Z: \\shares.oakland.edu\free
+NET USE Z: /delete >nul
+NET USE /persistent:yes Z: \\shares.oakland.edu\free >nul
 
-xcopy /s "Z:\Apportouninstall\Uninstall.vbs" "C:\Program Files\BizAppCenter\"
-xcopy /s "Z:\Apportouninstall\AppsToUninstall.txt" "C:\Program Files\BizAppCenter"
+xcopy /s "Z:\Apportouninstall\Uninstall.vbs" "C:\Program Files\BizAppCenter\" 
+xcopy /s "Z:\Apportouninstall\AppsToUninstall.txt" "C:\Program Files\BizAppCenter" 
 
 cscript "C:\Program Files\BizAppCenter\Uninstall.vbs"
-echo "Deleting BizAppCenter directory"
-rmdir /s /q "C:\Program Files\BizAppCenter"
+echo "Deleting BizAppCenter directory" >nul
+rmdir /s /q "C:\Program Files\BizAppCenter" >nul
 
-NET USE Z: /d
+NET USE Z: /d >nul
 echo "Uninstall complete"
 pause
